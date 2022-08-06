9b5121af94e79315371cea3498ceaecd601fff8b
MRAO0212 <mayureshvrao@gmail.com>
2022-08-06 16:36:26 -0700

    Move HTTP client logic to HTTPSpy Class

--- Feed/FeedTests/RemoteFeedLoaderTests.swift
+++ Feed/FeedTests/RemoteFeedLoaderTests.swift
@@ -9,29 +9,49 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "wwww.mayuresh.com")
+        HTTPClient.shared.get(from: URL(string: "www.mayuresh.com")!)
     }
 }
 
 class HTTPClient {
-    static let shared =  HTTPClient()
-    private init() {}
+    static var shared =  HTTPClient()
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    override func get(from url: URL!) {
+        requestedURL = url
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL(){
-        let client = HTTPClient.shared
+        //Given
+        let client = HTTPClientSpy()
+        
+        //When
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
+        
+        //Then
         XCTAssertNil(client.requestedURL)
         
     }
     
     func test_requestDatafromURL() {
-        let client = HTTPClient.shared
+        //Given
+        let client = HTTPClientSpy()
         let sut = RemoteFeedLoader()
+        HTTPClient.shared = client
+
+        
+        //When
         sut.load()
+        
+        //Then
         XCTAssertNotNil(client.requestedURL)
         
     }
