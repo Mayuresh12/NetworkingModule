ca399921cb93f99b874aff64139f6ea7e0d6a606
MRAO0212 <mayureshvrao@gmail.com>
2022-08-06 17:09:12 -0700

    Move `RemoteFeedLoader` to production

--- Feed/FeedTests/RemoteFeedLoaderTests.swift
+++ Feed/FeedTests/RemoteFeedLoaderTests.swift
@@ -6,24 +6,7 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-
-    init(client: HTTPClient, url: URL) {
-        self.client = client
-        self.url = url
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL!)
-}
+@testable import Feed
 
 class RemoteFeedLoaderTests: XCTestCase {
     
@@ -50,7 +33,8 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     //MARK: helpers
     
-    private func makeSUT(url: URL = URL(string: "www.test_requestDatafromURL.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "www.test_requestDatafromURL.com")!) -> (sut: RemoteFeedLoader,
+                                                                                         client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(client: client, url: url)
         return (sut, client)
