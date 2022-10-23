0b2ea82136953eadee2b0c6357da1d978e71e497
MRAO0212 <mayureshvrao@gmail.com>
2022-10-23 00:14:45 -0700

    create feedItem Struct

---
+++ EssentialFeed/EssentialFeed/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EssentialFeed
+//
+//  Created by Mayuresh Rao on 10/23/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
