e6ce58ef34e048736618fbbbfdcd75a19397baab
MRAO0212 <mayureshvrao@gmail.com>
2022-10-24 03:01:06 -0700

    RequestFeedloader doesnot request data upon creation.

---
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,24 @@
+//
+//  RemoteFeedLoaderTest.swift
+//  EssentialFeedTests
+//
+//  Created by Mayuresh Rao on 10/24/22.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    func test_init() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        XCTAssertNil(client.requestedURL)
+    }
+}
