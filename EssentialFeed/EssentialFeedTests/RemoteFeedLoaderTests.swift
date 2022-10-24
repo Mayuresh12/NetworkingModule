e12b1a03f306d5b71cb99fdda7ad17791461e4d7
MRAO0212 <mayureshvrao@gmail.com>
2022-10-24 03:13:15 -0700

    remote feed load request data on load

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,17 +8,32 @@
 import XCTest
 
 class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "www.mayuresh.com")
+    }
     
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_from_remoteURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
