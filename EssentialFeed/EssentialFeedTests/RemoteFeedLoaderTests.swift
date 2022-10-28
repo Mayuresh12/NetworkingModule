ecf5dd19f5e049a007b01d68555ee3878f9bc439
MRAO0212 <mayureshvrao@gmail.com>
2022-10-28 02:34:13 -0700

    delivers connectivity error on client error

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -23,6 +23,15 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        var capturedError: RemoteFeedLoader.Error?
+        client.error = NSError(domain: "Test", code: 0)
+        sut.load { error in capturedError = error
+        }
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // Helpers
     
     private func makeSUT(url: URL = URL(string: "www.mayureshvrao.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -32,9 +41,16 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
+  
         
-        func get(from url: URL) {
+        var requestedURLs = [URL]()
+        var error: Error?
+        
+        func get(from url: URL,
+                 completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
