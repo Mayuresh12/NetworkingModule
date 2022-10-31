a47e5df5bcd63221729cca0b49ece2c9080bc18f
MRAO0212 <mayureshvrao@gmail.com>
2022-10-31 01:27:06 -0700

    Delivers error on 200 Https status with invalid data

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -56,6 +56,16 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HttpResponseWithInvalidJSON(){
+        let (sut, client) = makeSUT()
+        var capturedError = [RemoteFeedLoader.Error]()
+        sut.load { capturedError.append($0)}
+        
+        let invalidJSON = Data(bytes: "invalid json".utf8)
+        client.complete(withStatus: 200,data: invalidJSON)
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
+    
     // Helpers
     
     private func makeSUT(url: URL = URL(string: "www.mayureshvrao.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -81,12 +91,14 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatus code: Int, at index: Int = 0) {
+        func complete(withStatus code: Int,
+                      data: Data = Data(),
+                      at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index],
                                            statusCode: code,
                                            httpVersion: nil,
                                            headerFields: nil)!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
 }
