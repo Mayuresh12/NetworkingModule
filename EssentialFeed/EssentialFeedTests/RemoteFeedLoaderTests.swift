764288c88fb54817411165ef2af30acd7714604e
MRAO0212 <mayureshvrao@gmail.com>
2023-01-12 01:42:51 -0800

    Move all logic to feedItemsMapper

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -94,10 +94,25 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     // Helpers
     
-    private func makeSUT(url: URL = URL(string: "www.mayureshvrao.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "www.mayureshvrao.com")!,
+    file: StaticString = #filePath,
+    line: UInt = #line) -> (sut: RemoteFeedLoader,
+                            client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(client: client, url: url)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
         return (sut, client)
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject,
+                                     file: StaticString = #filePath,
+                                     line: UInt = #line){
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, " Instance should have been deallocated. Potential memory leak.",
+                         file: file,
+                         line: line)
+        }
     }
     
     private func makeItem(id: UUID,
